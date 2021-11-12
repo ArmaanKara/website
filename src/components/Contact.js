4e1c5f237ee9ee3469c785f55c06032c2fc12445
Armaan Kara <vancouverarmaan@gmail.com>
2021-11-12 10:29:00 -0800

    added in react reveal scroll animations to skills and contact componenets

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade'
 import './Contact.css'; 
 
 export default function Contact(){
   return (
     <div className="contact">
           <div className="contact-container" id="contact">
+          <Fade left>
             <i className="far fa-user-circle" id="user-circle"></i>
             <h1 className="contact-header">
               What's Next?
@@ -17,16 +19,18 @@ export default function Contact(){
               Whether you may have a question, want to connect, or even just want to say hello, my inbox
               is always open and I will get back to you as soon as I can! Thank you.
             </p>
-            <a className="email-link" href="mailto:vancouverarmaan@gmail.com">
-              Contact Me
-            </a>
-            <div className="font-awesome-socials">
-              <a href="https://www.linkedin.com/in/armaankara"><i id="linkedin" className="fab fa-linkedin-in"></i></a>
-              <a href="https://github.com/ArmaanKara"><i className="fab fa-github" id="github"></i></a>
-              <a href="https://medium.com/@vancouverarmaan"><i id="medium" className="fab fa-medium-m"></i></a>
+            <div>
+              <a className="email-link" href="mailto:vancouverarmaan@gmail.com">
+                Contact Me
+              </a>
+              <div className="font-awesome-socials">
+                <a href="https://www.linkedin.com/in/armaankara"><i id="linkedin" className="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/ArmaanKara"><i className="fab fa-github" id="github"></i></a>
+                <a href="https://medium.com/@vancouverarmaan"><i id="medium" className="fab fa-medium-m"></i></a>
+              </div>
+            </div>
+          </Fade>
           </div>
-          </div>
-
     </div>
 
   )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import { skills } from "../data";
+import Fade from 'react-reveal/Fade'
 import './Skills.css';
 
 export default function Skills(){
     return (
         <section id="skills">
             <div className="skills-container">
+                <Fade right>
                 <div className="skills-text">
                     <i class="fas fa-toolbox" id="toolbox-icon" />
                     <h1 className="h1-text-skill-technologies">
@@ -16,6 +18,8 @@ export default function Skills(){
                         Here are my most used languages, libraries, and technologies to date.
                     </p>
                 </div>
+                </Fade>
+                <Fade bottom>
                 <div className="skills-data">
                     {skills.map((skill) => (
                         <div key={skill} className="actual-skill-text">
@@ -28,6 +32,7 @@ export default function Skills(){
                         </div>
                     ))}
                 </div>
+                </Fade>
             </div>
         </section>
     );
