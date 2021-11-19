fa3c25224365ffb695f1cc216590b17f318a350b
Armaan Kara <vancouverarmaan@gmail.com>
2021-11-18 17:01:29 -0800

    updated projects section with three new projects, still waiting on CRUD app to finish then will input

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { CodeIcon } from "@heroicons/react/solid";
-import { projects } from "../data";
+import { projects, projects2 } from "../data";
 import './Projects.css';
 
 export default function Projects() {
@@ -19,6 +19,24 @@ export default function Projects() {
                 </div>
                 <div className="projects-gallery">
                     {projects.map((project) => (
+                        <a href={project.link} key={project.image} className="project-links">
+                            <div className="project1">
+                                <img alt="gallery" className="project-image" src={project.image}/>
+                            <div className="card-flipped-project1-subtitle">
+                                <h2 className="card-flipped-project1-subtitle-yes">
+                                    {project.subtitle}
+                                </h2>
+                                <h1 className="card-flipped-project1-title">
+                                    {project.title}
+                                </h1>
+                                <p className="card-flipped-project1-description">{project.description}</p>
+                            </div>
+                            </div>
+                        </a>
+                    ))}
+                </div>
+                <div className="projects-gallery">
+                    {projects2.map((project) => (
                         <a href={project.link} key={project.image} className="project-links">
                             <div className="project1">
                                 <img alt="gallery" className="project-image" src={project.image}/>
--- src/data.js
+++ src/data.js
@@ -1,16 +1,16 @@
 export const projects = [
   {
     title: "Rate My Residence",
-    subtitle: "React.js",
+    subtitle: "React, Node.js, Express, and MongoDB",
     description: "Currently working on a campus-wide residence ranking web app for the University of British Columbia.",
     image: "./media/rmr.gif",
     link: "https://happy-curie-7161ee.netlify.app",
   },
   {
     title: "Resonance Consulting",
-    subtitle: "HTML, CSS and React",
+    subtitle: "React and Netlify",
     description:
-      "Currently designing and building a custom React website for the consutling firm Resonance. The website is hosted on Netlify",
+      "Designed and built a responsive React website for the consutling firm Resonance Consulting. The website is hosted on Netlify",
     image: "./media/resonance.gif",
     link: "https://www.resonance-co.com/#/",
   },
@@ -21,6 +21,31 @@ export const projects = [
       "Imported data from Yahoo Finance to analyze and then create a portfolio of four stocks to invest in based on their risk and past performance. ",
     image: "./media/portfoliooptimizationgithub.gif",
     link: "https://github.com/ArmaanKara/PortfolioOptimization",
+  },
+];
+
+export const projects2 = [
+  {
+    title: "Fall 2021 Tesla Clone",
+    subtitle: "React, Redux and Styled Components",
+    description: "Built a clone Tesla's website with responsive Navbar, a link to Tesla's 2020 Impact Report, and on-scroll animations from React Reveal",
+    image: "./media/teslaclone.gif",
+    link: "https://tesla-clone-ak.herokuapp.com/",
+  },
+  {
+    title: "FullStack Grocery List App",
+    subtitle: "React, Node, Express & MongoDB (MERN)",
+    description: "Built a fully functional CRUD app with the MERN stack so that you can be better prepared the next time you go grocery shopping. I always find myself forgetting to but certain items because I don't make a list. Now I will never forget an item thanks to my handy grocery shopping list webapp!",
+    image: "",
+    link: "",
+  },
+  {
+    title: "AI vs Human Tic Tac Toe",
+    subtitle: "React Hooks and Github pages",
+    description:
+      "This fun project challenged me to think outside the box with React Hooks and create a fictional AI to play against. Give it a try!",
+    image: "./media/tictactoe.gif",
+    link: "https://armaankara.github.io/tictactoe/",
   },
 ];
 
